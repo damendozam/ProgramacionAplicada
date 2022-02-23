0b948446cf69c5f3fddd1668bce4aa24be147195
Danielamartine <kmartinezmo01@uninpahu.edu.co>
2022-02-22 19:58:48 -0500

    actualizacion de comentarios

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
+from json.tool import main
+from animals import Animal ,Raza , perro ,Textura , pez 
+
+Animal1=Animal ("rex","tiburon","15gramos","azul")
+print("loro")
+
+
+
+Animal2=perro ("murdoc",)
+animal3=Textura ()
+Animal4=pez ()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
