26b5f541529f961c0402b608480e3b999b34b3b8
Mafe14Ortiz <mfiravitobaor@uninpahu.edu.co>
2022-02-24 19:52:49 -0500

    datos de peces

--- animals.py
+++ animals.py
@@ -4,16 +4,13 @@ from typing_extensions import Self
 
 
 class Animal(object):
-    habitat=""
-    alimentacion=""
     def __init__(self,nombre,especie,peso,color):
         self.nombre=nombre
         self.especie=especie
         self.peso=peso
-        self.color=color
-    def comunicacion(self):
-        print("el animal se comunica")
-        
+        self.color=color       
+    def comunicacion(self,comunicacion):
+        self.comunicacion = comunicacion       
     def desplazamiento(self,desplazamiento):
         self.desplazamiento = desplazamiento
     def alimentacion(self,alimentacion):
@@ -21,7 +18,7 @@ class Animal(object):
     def habitat(self, habitat):
         self.habitat = habitat
 
-class Raza(object):
+class Raza():
     def __init__(self):
          pass
     def begin(self,pitbull,labrador,dalmata):
@@ -30,7 +27,7 @@ class Raza(object):
         self.dalmata = dalmata
          
 
-class Textura(object):
+class Textura():
     def __init__(self):
         pass
     def begin(self,escamas,manchas, rayas, melenuda):
@@ -48,11 +45,12 @@ class pez(Animal):
         self.color= color
         self.especie = especie
         self.peso =peso
+
 class perro(Animal,Raza):
-     def __init__(self, nombre, especie, peso, color):
+     def __init__(self, nombre, raza, peso, color):
          self.color=color
          self.nombre=nombre
-         self.especie=especie
+         self.raza =raza
          self.peso=peso
          
 def getNombre(self):
@@ -68,9 +66,29 @@ def mostrarAnimal(self):
     print("\nnombre: "+self.getNombre()+"\nespecie: "+self.getEspecie()+"\npeso: "+self.getPeso()+"\ncolor: "+self.getColor())
 
          
+nombre = raw_input("Ingrese el nombre: ")
+especie = raw_input("Ingrese que tipo de especie es el animal: ")
+peso = raw_input("Ingrese el peso del animal: ")
+color = raw_input("Describa el color del animal: ")
 
-          
-                    
+a =Animal(nombre,especie,peso,color)
+a.mostrarAnimal()
+
+perro1 = Perro("Firulais","pitbull","20kg","blanco con cafe")
+perro2 = Perro("Mateo","labrador","30kg","amarillo")
+perro3 = Perro("Toby","Bulldog","24kg","Blanco")
+
+pez1 = Pez("Nemo","pez dorado","0,5kg","amarillento-anaranjado")
+pez2 = Pez("Pez betta","pez betta cola de rosa","0.8kg","rosado y blanco" )
+pez3 = Pez("Apolo","guppy","10gramos","azul y blanco")
+
+print("Mi nombre es",perro1.nombre,"mi raza es",perro1.raza,"mi peso es de",perro1.peso,"soy de color",perro1.color)
+print("Mi nombre es",perro2.nombre,"mi raza es",perro2.raza,"mi peso es de",perro2.peso,"soy de color",perro2.color)
+print("Mi nombre es",perro3.nombre,"mi raza es",perro3.raza,"mi peso es de",perro3.peso,"soy de color",perro3.color)
+
+print("Mi nombre es",pez1.nombre,"mi especie es",pez1.especie,"mi peso es de",pez1.peso,"soy de color",pez1.color)
+print("Mi nombre es",pez2.nombre,"mi especie es",pez2.especie,"mi peso es de",pez2.peso,"soy de color",pez2.color)   
+print("Mi nombre es",pez3.nombre,"mi especie es",pez3.especie,"mi peso es de",pez3.peso,"soy de color",pez.color)                  
  
 
     
