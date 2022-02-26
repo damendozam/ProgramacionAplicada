ca4e5c35191293d4ec18b1d894c14c85606243d1
Danielamartine <kmartinezmo01@uninpahu.edu.co>
2022-02-25 22:35:11 -0500

    commit message

--- index.py
+++ index.py
@@ -1,16 +1,38 @@
 from json.tool import main
-from animals import Animal ,Raza , perro ,Textura , pez 
+from os import name
+from typing_extensions import Self
+from animals import Animal 
+from animals import Raza
+from animals import perro
+from animals import Textura
+from animals import pez
 
+if __name__ == "main" :
+    print ("Animal")
 Animal1=Animal ("rex","tiburon","15gramos","azul")
-print("loro")
+print (Animal1)
+listatest=[Animal1]
 
-
-
-Animal2=perro ("murdoc",)
-animal3=Textura ()
-Animal4=pez ()
+def  __str__(self):
+        return "{}, nombre: {}, especie: {} , {} peso {} color ".format(type(self).__name__,self.nombre,self.especie,Self.color,
+    )
     
+def __repr__(self):
+        return self.__str__()
 
+
+Animal2=perro ("murdoc","gramde","150kilogramos","maron")
+print(Animal1)
+listatest = [Animal2]
+animal3=Textura (perro)
+Animal4= pez ("nemo","grande","1202","naranja")
+print (Animal4)
+listatest =[Animal4]
+
+if __name__ == "main" :
+    print ("Animal")
+
+    
 
 
 
---
+++ index2.py
@@ -0,0 +1,84 @@
+from json.tool import main
+from os import name
+from sys import orig_argv
+from typing_extensions import Self
+from animals import Animal 
+from animals import Raza
+from animals import perro
+from animals import Textura
+from animals import pez
+
+if __name__ == "main" :
+    print ("Animal")
+Animal1=Animal ("rex","tiburon","15gramos","azul")
+print (Animal1)
+listatest=[Animal1]
+
+def  __str__(self):
+        return "{}, nombre: {}, especie: {} , {} peso {} color ".format(type(self).__name__,self.nombre,self.especie,Self.color,self,peso
+        )
+    
+def __repr__(self):
+        return self.__str__()
+
+
+Animal2=perro ("murdoc","gramde","150kilogramos","maron")
+print(Animal1)
+listatest = [Animal2]
+
+def  __str__(self):
+    return "{}"", color: {}, nombre {} , {} especie {} peso ".format(type(self).__name__,self.color,self.nombre,self.especie,self.peso)
+
+def __repr__(self):
+    return self.__str__()
+    
+
+animal3=Textura (perro)
+Animal4= perro ("nemo","liso","150kg","cafesito")
+print (Animal4)
+listatest =[Animal4]
+
+def __Str__(self):
+    return  "{}"", nombre: {}, especie {} , {} peso {} color ".format(type(self).__name__,self.nombre,self.especie,self.peso,self.color)
+
+def __str__(Self):
+    return Self.__str__()
+
+
+animal4=pez("rock","pez bella","10kkg","azul")
+print(animal4)
+listatest = [animal4]
+
+def __str__(self):
+    return  "{}"", nombre: {}, especie {} , {} peso {}  color ".format(type(self).__name__,self.nombre,self.especie,self.peso,self.color)
+ 
+def __Str__(self):
+    return self.__str__()
+
+ListaAnimal=[pez,perro,]
+print(ListaAnimal)
+listatest = [animal4]
+
+listaAnimales=[pez,perro,]
+print(listaAnimales)
+listatest =[listaAnimales]
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
